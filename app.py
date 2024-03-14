ff5836056f4fe5436cc369d8fdfe41a03fde4a50
YasaminAbbaszadegan <Yasamin.abbaszadegan@ryerson.ca>
2024-03-14 11:13:37 -0400

    add node parser options

--- app.py
+++ app.py
@@ -8,38 +8,58 @@ from file_operations import cache_uploaded_documents, remove_uploaded_folder
 st.set_page_config(page_icon="./icon/Toronto-logo-blue-750.jpg")
 st.image("./icon/Toronto-logo-blue-750.jpg", use_column_width=True, width=30)
 st.title('Zoning Decision Assistant: City Notice Reader')
-
 upload_folder = 'temp'
 if upload_folder and not os.path.exists(upload_folder):
     os.makedirs(upload_folder)
 
+# Display file uploader for multiple documents
 uploaded_files = st.file_uploader("Upload multiple documents", accept_multiple_files=True)
 first_button_run_finished = False
 
+# If files are uploaded and upload folder is specified
 if uploaded_files and upload_folder:
+    # Cache the uploaded documents to the specified folder
     cached_documents = cache_uploaded_documents(uploaded_files, upload_folder)
+    # Get paths of the uploaded documents
     document_paths = [f"./{upload_folder}/" + file_content for file_content in os.listdir(f'./{upload_folder}/')]
-    sentence_window_engines = initialize_llama(document_paths)
+    
+    # Display buttons for selecting node parser options
+    parser_option = st.radio("Select Node Parser Option:", ("Basic Parser", "Sentence Window Parser"))
+
+    # Set flag based on user's choice of node parser
+    use_advanced_node_parser = True if parser_option == "Sentence Window Parser" else False
+
+    # Initialize the LLAMA model and query engines for the uploaded documents
+    sentence_window_engines = initialize_llama(document_paths, use_advanced_node_parser)
+    # Get user input query
     query = st.text_input("What would you like to ask?")
 
+    # If 'Submit Query' button is clicked
     if st.button("Submit Query"):
+        # Check if query is provided
         if not query.strip():
             st.error(f"Please provide the search query.")
         else:
             try:
                 combined_responses = {}
+                # Query each document using corresponding query engine
                 for doc_title, engine in sentence_window_engines.items():
                     response = engine.query(query)
                     combined_responses[doc_title] = response.response
 
+                # Display the combined responses
                 st.write(combined_responses)
+                # Convert responses to DataFrame and display
                 df = pd.DataFrame.from_dict(combined_responses, orient='index', columns=['Response'])
                 st.dataframe(df)
             except Exception as e:
                 st.error(f"An error occurred: {e}")
 
+    # Update flag to indicate the first button run is finished
     first_button_run_finished = True
 
+# If the first button run is finished, show the "Remove Uploaded Folder" button
 if first_button_run_finished:
     if st.button("Remove Uploaded Folder"):
+        # Remove the uploaded folder directory
         remove_uploaded_folder(upload_folder)
--- initialize_llama.py
+++ initialize_llama.py
@@ -1,4 +1,6 @@
 # initialize_llama.py
+from llama_index.core.node_parser import SentenceWindowNodeParser, SentenceSplitter
+
 import os
 from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryReader, Document
 from llama_index.core.node_parser import SentenceWindowNodeParser
@@ -6,23 +8,65 @@ from llama_index.llms.llama_cpp import LlamaCPP
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core.indices.postprocessor import SentenceTransformerRerank, MetadataReplacementPostProcessor
 
-def initialize_llama(documents_paths):
+# def initialize_llama(documents_paths):
+#     """
+#     Initializes the LLAMA model and creates query engines for each document.
+
+#     Args:
+#         documents_paths (list): List of file paths of the documents to be indexed.
+
+#     Returns:
+#         dict: Dictionary containing document titles as keys and corresponding query engines as values.
+#     """
+#     sentence_window_engines = {}
+#     llm = LlamaCPP(model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf', 
+#                    temperature=1, 
+#                    model_kwargs={"n_gpu_layers": -1},
+#                    verbose=True,)
+#     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+#     node_parser = SentenceWindowNodeParser.from_defaults(window_size=3, window_metadata_key="window", original_text_metadata_key="original_text")
+#     sentence_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, node_parser=node_parser)
+#     rerank = SentenceTransformerRerank(top_n=2, model="BAAI/bge-reranker-base")
+#     postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
+    
+#     for document_path in documents_paths:
+#         title = os.path.splitext(os.path.basename(document_path))[0]
+#         documents = SimpleDirectoryReader(input_files=[document_path]).load_data()
+#         documents = Document(text="\n\n".join([doc.text for doc in documents]))
+#         sentence_index = VectorStoreIndex.from_documents([documents], service_context=sentence_context)
+#         sentence_window_engine = sentence_index.as_query_engine(similarity_top_k=6, node_postprocessors=[postproc, rerank])
+#         sentence_window_engines[title] = sentence_window_engine
+
+#     return sentence_window_engines
+
+
+
+# Function to initialize LLAMA model and necessary components
+def initialize_llama(documents_paths, use_advanced_node_parser):
     """
     Initializes the LLAMA model and creates query engines for each document.
 
     Args:
         documents_paths (list): List of file paths of the documents to be indexed.
+        use_advanced_node_parser (bool): Flag indicating whether to use advanced node parser or not.
 
     Returns:
         dict: Dictionary containing document titles as keys and corresponding query engines as values.
     """
+
     sentence_window_engines = {}
     llm = LlamaCPP(model_url='https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q4_K_M.gguf', 
                    temperature=1, 
                    model_kwargs={"n_gpu_layers": -1},
                    verbose=True,)
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-    node_parser = SentenceWindowNodeParser.from_defaults(window_size=3, window_metadata_key="window", original_text_metadata_key="original_text")
+
+    # Select node parser based on user's choice
+    if use_advanced_node_parser:
+        node_parser = SentenceWindowNodeParser.from_defaults(window_size=10, window_metadata_key="window", original_text_metadata_key="original_text")
+    else:
+        node_parser = SentenceSplitter() #chunk_size=500, chunk_overlap=20
+    
     sentence_context = ServiceContext.from_defaults(llm=llm, embed_model=embed_model, node_parser=node_parser)
     rerank = SentenceTransformerRerank(top_n=2, model="BAAI/bge-reranker-base")
     postproc = MetadataReplacementPostProcessor(target_metadata_key="window")
